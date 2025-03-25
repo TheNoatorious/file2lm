201343eee33c05402e9f71d655204f4d684e8b6a
N.Z. Timisela <noatimisela@gmail.com>
2025-03-25 13:08:19 +0100

    Add: Document extraction functionality

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader
 
 load_dotenv()
+
+loader = TextLoader("./assets/facts.txt")
+docs = loader.load() # Extract all content from the loader
+
+print(docs) # Output of the docs
+
